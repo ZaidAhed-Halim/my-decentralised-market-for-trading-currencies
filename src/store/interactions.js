df98c9c6172b273b86fe3446574c8fe60724dd75
zaid halim <zaid_abd.alhalim@yahoo.com>
2021-02-02 15:46:47 +0200

    adding the project to github

---
+++ src/store/interactions.js
@@ -0,0 +1,90 @@
+import Web3 from 'web3'
+import {
+    web3Loaded ,
+    web3AccountLoaded,
+    tokenLoaded,
+    exchangeLoaded,
+    cancelledOrdersLoaded,
+    filledOrdersLoaded,
+    allOrdersLoaded,
+    orderCancelling,
+    orderCancelled
+} from './actions'
+
+import Token from '../abi/Token.json'
+import Exchange from '../abi/Exchange.json'
+
+export const loadWeb3 = (dispatch)=> {
+    const web3 = new Web3(window.ethereum)
+    dispatch(web3Loaded(web3))
+    return web3
+}
+
+export const loadAccount = async (web3,dispatch)=> {
+    const accounts = await web3.eth.getAccounts()
+    const account = accounts [0]
+    dispatch(web3AccountLoaded(account))
+    return account
+}
+
+export const loadToken = async (web3,networkId ,dispatch) => {
+    try {
+        const token = new web3.eth.Contract(Token.abi , Token.networks[networkId] .address)
+        dispatch(tokenLoaded(token))
+        return token
+    } catch (error) {
+        console.log('contract not deployed to he current network . pelase select another network with metamsk')
+        return null
+    }
+}
+
+export const loadExchange = async (web3,networkId ,dispatch) => {
+    try {
+        const exchange = new web3.eth.Contract(Exchange.abi , Exchange.networks[networkId] .address)
+        dispatch(exchangeLoaded(exchange))
+        return exchange
+    } catch (error) {
+        console.log('contract not deployed to he current network . pelase select another network with metamsk')
+        return null
+    }
+}
+
+export const loadAllOrders = async (exchange,dispatch) => {
+
+    // Fetch cancelled orders with the "cancel" evet stream
+    const cancelStream = await exchange.getPastEvents('Cancel', {fromBlock:0,toBlock:'latest'})
+    // format cancelled orders
+    const cancelledOrders = cancelStream.map((event)=>event.returnValues)
+    // add cancelled orders to the redux store 
+    dispatch(cancelledOrdersLoaded(cancelledOrders))
+    // Fethch filled orders with the "trade" event stream 
+    const tradeStream = await exchange.getPastEvents ('Trade' ,{fromBlock :0 , toBlock :'latest'})
+    // format filled orders
+    const filledOrders = tradeStream.map((event) =>event.returnValues)
+    // add cancelled orders to the redux store 
+    dispatch(filledOrdersLoaded(filledOrders))
+    
+    // load order stream 
+    const orderStream = await exchange.getPastEvents('Order' ,{fromBlock:0 , toBlock:'latest'})
+    // format order stream
+    const allOrders = orderStream.map((event) => event.returnValues)
+    // add open orders to the redux store
+    dispatch (allOrdersLoaded(allOrders))
+}
+
+export const cancelOrder= (dispatch,exchange,order,account) => {
+    exchange.methods.cancelOrder(order.id).send({from : account})
+    .on('transactionHash',(hash) => {
+        dispatch(orderCancelling())
+    })
+    .on('error' ,(error) => {
+        console.log(error)
+        window.alert ('There was an error !')
+    })
+}
+
+export const subscribeToEvents = async (exchange, dispatch) => {
+    exchange.events.Cancel({}, (error, event) => {
+      dispatch(orderCancelled(event.returnValues))
+    })
+  }
